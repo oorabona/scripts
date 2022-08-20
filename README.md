a81e546346443d0a72680ed1591c80fb39cd3185
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-08-20 20:56:38 +0200

    :rocket: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# scripts
-Collection of scripts that may be useful to share
+# Scripts
+
+Collection of scripts that may be useful to share, or to be used as a starting point for your own scripts.
+
+## Installation and usage
+
+List of scripts and their installation instructions.
+[Update Debian APT sources.list file](debian/update-sources-list.sh.md)
+
+# License
+
+MIT License [here](LICENSE)
---
+++ debian/update-sources-list.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# Update sources.list file properly and easily
+# Heavily inspired by the work of: netselect-apt by Avery Pennarun (https://github.com/apenwarr/netselect)
+
+# Current patterns taken from https://wiki.debian.org/SourcesList
+# with the use of the Fastly CDN instead of seeking for the closest mirror.
+
+# deb http://deb.debian.org/debian bullseye main contrib non-free
+# deb-src http://deb.debian.org/debian bullseye main contrib non-free
+# 
+# deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+# deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+# 
+# deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+# deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
+
+host="http://deb.debian.org/debian"
+want_security=${WANT_SECURITY:-1}
+want_updates=${WANT_UPDATES:-1}
+want_sources=${WANT_SOURCES:-0}
+want_nonfree=${WANT_NONFREE:-0}
+want_contrib=${WANT_CONTRIB:-0}
+outfile="sources.list"
+release=${1:-$(lsb_release -sc)}
+arch=$(/usr/bin/dpkg --print-architecture)
+
+options="-o a:so:ncuh -l arch:,sources,outfile:,nonfree,contrib,updates,help"
+
+# misc functions
+log()
+{
+	echo "$@" >&2
+}
+
+usage()
+{
+	log "Usage: $0 [OPTIONS] [ Release Codename ]"
+    log ""
+    log "Release Codename is optional and defaults to the current release."
+    log "Otherwise it refers to the release you want to generate the sources.list for."
+    log "e.g. $0 buster"
+    log "e.g. $0 stable"
+    log ""
+	log "Options:"
+    log "   -a, --arch             Use mirrors containing arch (default: $arch)"
+	log "   -s, --sources          Include deb-src lines in generated file (default: no)"
+	log "   -o, --outfile OUTFILE  Use OUTFILE as the output file"
+	log "                            (default: sources.list)"
+	log "   -n, --nonfree          Use also non-free packages in OUTFILE (default: no)"
+    log "   -c, --contrib          Use contributed packages (default: no)"
+    log "   -u, --updates          Use updates (default: yes)"
+    log "   -S, --security         Use security updates (default: yes)"
+    log "   -h, --help             Display this help"
+}
+
+
+# commandline parsing
+temp=$(getopt $options -n 'update-sources-list.sh' -- "$@")
+if [ $? != 0 ]; then echo "Terminating..." >&2; exit 2; fi
+eval set -- "$temp"
+while true; do
+	case "$1" in
+        -a|--arch) arch=$2; shift 2 ;;
+		-s|--sources) want_sources=1; shift ;;
+		-n|--nonfree) want_nonfree=1; shift ;;
+        -c|--contrib) want_contrib=1; shift ;;
+        -u|--updates) want_updates=1; shift ;;
+        -S|--security) want_security=1; shift ;;
+        -h|--help) usage; exit 0;;
+		--) shift; break;;
+		*) echo "Internal Error!"; echo "args: $@"; exit 1;;
+	esac
+done
+
+# netselect starting
+log "Using distribution $release."
+
+log "Writing $outfile."
+
+if [ -f "$outfile" ]; then
+	log "$outfile exists, moving to $outfile.$(date +%s)"
+	mv $outfile $outfile.orig
+fi
+
+stream=","
+if [ "$want_security" -eq 1 ]; then
+    stream="${stream}-security"
+fi
+if [ "$want_updates" -eq 1 ]; then
+    stream="${stream},-updates"
+fi
+
+if [ "$want_contrib" -eq 1 ]; then
+    extra="${extra} contrib"
+fi
+if [ "$want_nonfree" -eq 1 ]; then
+    extra="${extra} non-free"
+fi
+
+IFS=,
+(
+	echo "# Debian packages for $release"
+    for s in $stream; do
+        echo -n "deb ${host}"
+        if [ "$s" = "-security" ]; then
+            echo -n "$s"
+        fi
+        echo " ${release}${s} main ${extra}"
+        if [ "$want_sources" -eq 1 ]; then
+            echo -n "deb-src ${host}"
+            if [ "$s" = "-security" ]; then
+                echo -n "$s"
+            fi
+            echo " ${release}${s} main ${extra}"
+        fi
+    done
+) > $outfile
