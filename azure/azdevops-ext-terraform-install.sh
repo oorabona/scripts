b28b7c8dc6ecfd42597bc03f4acdfa2da4ef6f28
Olivier ORABONA <oorabona@users.noreply.github.com>
2023-02-03 00:42:14 +0100

    remove extra apt get

--- azure/azdevops-ext-terraform-install.sh
+++ azure/azdevops-ext-terraform-install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Install Terraform
 # https://learn.hashicorp.com/tutorials/terraform/install-cli
@@ -15,10 +15,9 @@ fi
 # Avoid having to create a script and host it 'somewhere'
 # See https://learn.hashicorp.com/tutorials/terraform/install-cli
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository -y \"deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main\"
+sudo apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install -y terraform=${terraform_version}
 
 # TODO define version
-sudo apt-get install -y 
 curl https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
 
